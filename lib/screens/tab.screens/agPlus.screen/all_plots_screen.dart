e26d97e3cafe3014a49ae5ea05d73567d83a9b13
Aman kumar Pandey <paman4681@gmail.com>
2026-05-13 19:54:03 +0530

    asdf

--- lib/screens/tab.screens/agPlus.screen/all_plots_screen.dart
+++ lib/screens/tab.screens/agPlus.screen/all_plots_screen.dart
@@ -117,7 +117,8 @@ class AllPlotsScreen extends HookWidget {
                                               borderRadius: BorderRadius.circular(12),
                                               child: CachedNetworkImage(
                                                 imageUrl:
-                                                    "https://images.unsplash.com/photo-1500382017468-9049fed747ef?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                                                "https://agrichikitsabucket.s3.ap-south-1.amazonaws.com/app-assets/empty-plot.png",
+                                                    // "https://images.unsplash.com/photo-1500382017468-9049fed747ef?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                                                 fit: BoxFit.cover,
                                                 placeholder: (context, url) => Skeleton(
                                                   height: dimension['height']! * 0.25,
