5870f1ffb400768e8e5bdbdab690b15d4df0cc0e
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2026-06-28 17:11:19 +0530

    sdfghjm

--- lib/screens/tab.screens/agPlus.screen/helper/selected_plot_details.dart
+++ lib/screens/tab.screens/agPlus.screen/helper/selected_plot_details.dart
@@ -205,7 +205,7 @@ class SelectedPlotDetails extends HookWidget {
               borderRadius: BorderRadius.circular(12),
               child: CachedNetworkImage(
                 imageUrl: selectedPlot.cropImage ??
-                    "https://images.unsplash.com/photo-1593738226658-f3e01177c3f0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://agrichikitsabucket.s3.ap-south-1.amazonaws.com/app-assets/empty-plot.png",
                 fit: BoxFit.cover,
                 placeholder: (context, url) => Skeleton(
                   height: dimension['height']! * 0.21,
