30daf8849532ba9310431d1776d435643eda7a46
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2026-06-28 11:59:38 +0530

    fix: url and versionCode Updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.freshnic.agriChikitsa.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 16
+        versionCode = 20
         versionName = flutter.versionName
         multiDexEnabled = true
         ndk {
