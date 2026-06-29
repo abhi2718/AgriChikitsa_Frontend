d0e6dcf6795f340a280ae2b11f74947db13b78ad
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2026-06-29 08:09:30 +0530

    asdf

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.freshnic.agriChikitsa.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 25
+        versionCode = 28
         versionName = flutter.versionName
         multiDexEnabled = true
         ndk {
